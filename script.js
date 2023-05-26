8d5904d9899c266b7e7ce0498b677f8a87c62f57
Mir Nushrat <nusrat.lilac@gmail.com>
2023-05-26 13:56:54 +0600

    completed add task to the task list and design a span for removing task

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
+const listContainer = document.getElementById("list-container");
+const inputBox = document.getElementById("input-item")
 
+function addTask(){
+    if(inputBox.value === "")  {
+        alert("You must write something!");
+    } else {
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+
+}
 
